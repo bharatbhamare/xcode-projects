f24298d59074debc96c2b9b051004b6eca3d014b
bharat.bhamare <yogesh.khaire@triarqhealth.com>
2018-11-01 16:06:24 +0530

    demo commit

--- GitDemo/GitDemo/ViewController.swift
+++ GitDemo/GitDemo/ViewController.swift
@@ -13,9 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("modification")
+        print("demi cahanges")
         // Do any additional setup after loading the view, typically from a nib.
+        demofunc(name: "bharat");
     }
-
+    func demofunc(name:String)->String
+    {
+        return String(name.reversed());
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
