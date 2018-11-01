5cca245d85397e53c99179aca645b45f95686f60
bharat.bhamare <yogesh.khaire@triarqhealth.com>
2018-11-01 14:19:42 +0530

    first commit

---
+++ GitDemo/GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Mac2 on 11/1/18.
+//  Copyright © 2018 Mac2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
